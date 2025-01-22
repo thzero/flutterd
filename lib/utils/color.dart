67f51fbc02ccc472538432f835c0a679ddc620a5
Corvette Rox <development@thzero.com>
2025-01-22 11:05:59 -0600

    fix deprecation

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/rendering.dart';
 
 extension ColorX on Color {
-  String toHexTriplet() => '#${(value & 0xFFFFFF).toRadixString(16).padLeft(6, '0').toUpperCase()}';
+  // String toHexTriplet() => '#${(value & 0xFFFFFF).toRadixString(16).padLeft(6, '0').toUpperCase()}';
+  String toHexTriplet() => '#'
+      '${(a * 255).round().toRadixString(16).padLeft(2, '0').toUpperCase()}'
+      '${(r * 255).round().toRadixString(16).padLeft(2, '0').toUpperCase()}'
+      '${(g * 255).round().toRadixString(16).padLeft(2, '0').toUpperCase()}'
+      '${(b * 255).round().toRadixString(16).padLeft(2, '0').toUpperCase()}';
 }
